2832626460898bb5844520f15855b09f4bfa8b62
Camilasanches07 <camilasanchesferreira@hotmail.com>
2024-11-27 18:03:43 -0300

    atualizações

--- app.py
+++ app.py
@@ -7,8 +7,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     alunos_csv = pd.read_csv("./data/alunos.csv")
-    print(alunos_csv)
-    return render_template("index.html", result = alunos_csv)
+    alunos_data = []
+
+    for index, row in alunos_csv.iterrows():#Para percorrer linhas
+        alunos_data.append({
+            "Identificação do Aluno": row["id_aluno"],
+            "Nome": row["nome"],
+            "Identificação da Sala": row["salas_id"]
+        })
+    print(alunos_data)    
+    return render_template("index.html", result = alunos_data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
