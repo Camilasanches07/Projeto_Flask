5f696ba62aff59f92f98a1c7ddb625572269f98d
Camilasanches07 <camilasanchesferreira@hotmail.com>
2024-12-02 13:50:31 -0300

    atualizações

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ def index():
     return render_template("index.html", result = alunos_data)
 
 
+@app.route("/professores")
+def professores():
+     return render_template("professores.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
     
